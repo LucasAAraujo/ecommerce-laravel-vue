a86bc23224c0f290caa041fc3c05dc16f12e0776
Lucas Alves <lucasalves3.0@hotmail.com>
2024-10-14 21:04:28 -0300

    update: vuex e vue router instalado e configurado

---
+++ backend/src/store/index.js
@@ -0,0 +1,12 @@
+import {createStore} from "vuex";
+
+const store = createStore({
+  state: {
+    test: "1, 2, 3"
+  },
+  getters: {},
+  actions: {},
+  mutations: {}
+})
+
+export default store;
